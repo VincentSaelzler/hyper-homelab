5b968d8d543e58c1e8f014c57a31e5452f97615d
Vincent Saelzler <vincent.saelzler@gmail.com>
2021-09-22 22:13:18 -0400

    stash stuff to work on cloud-init apprach

--- provision/SaveImage.ps1
+++ provision/SaveImage.ps1
@@ -3,10 +3,16 @@
 
 #Invoke-WebRequest https://cloud-images.ubuntu.com/hirsute/current/hirsute-server-cloudimg-amd64-azure.vhd.zip -UseBasicParsing
 #Invoke-WebRequest "https://cloud-images.ubuntu.com/hirsute/current/hirsute-server-cloudimg-amd64-azure.vhd.zip" -UseBasicParsing -OutFile "hirsute-server-cloudimg-amd64-azure.vhd.zip"
-
-
 #Expand-Archive -LiteralPath "hirsute-server-cloudimg-amd64-azure.vhd.zip" -DestinationPath .
 
+$Before = "C:\Users\vince\src\hyper-homelab\provision\livecd.ubuntu-cpc.azure.vhd"
+$After = "C:\Users\vince\src\hyper-homelab\provision\livecd.ubuntu-cpc.azure.vhdx"
+#Convert-VHD -Path livecd.ubuntu-cpc.azure.vhd –DestinationPath c:\livecd.ubuntu-cpc.azure.vhdx -WhatIf
+Convert-VHD -Path $Before -DestinationPath $After #-WhatIf
+
+"C:\Users\Public\Documents\Hyper-V\Virtual hard disk templates"
 
 
-#Convert-VHD –Path d:\VM01\Disk0.vhd –DestinationPath d:\VM01\Disk0.vhdx
+#Write-Host $Before$After
+
+#TODO: Enable secure boot
