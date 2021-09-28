eefb719e3890d2078039a290b9f9c4f754449f58
Vincent Saelzler <vincent.saelzler@gmail.com>
2021-09-27 22:47:41 -0400

    vhd resize and GUI install

--- provision/CreateVm.ps1
+++ provision/CreateVm.ps1
@@ -1,16 +1,22 @@
 $VMName = "27"
 $SwitchName = "TheOutside"
+$VHDSize = 8589934592 # that's 8 GiB
+$VHDFile = "$VMName-os.vhdx"
 
 $VM = @{
      Name = "$VMName"
      Generation = 2
      SwitchName = $SwitchName
-     VHDPath = "$VMName-os.vhdx"
- }
+     VHDPath = $VHDFile
+}
+
+Resize-VHD -Path $VHDFile -SizeBytes $VHDSize
 
 New-VM @VM
 Set-VMFirmware "$VMName" -EnableSecureBoot Off
 Add-VMDvdDrive "$VMName" -Path "$VMName-seed.iso"
+
+Start-VM $VMName
 
 #can't seem to make this work but it might not matter
 #check if the router forwards via IP before wasting more time.
