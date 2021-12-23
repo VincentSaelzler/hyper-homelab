f3d61847874f931149f473f6700ae6d15004a7e1
Vincent Saelzler <vincent.saelzler@gmail.com>
2021-12-23 01:07:40 -0500

    pvs and vgs with ansible

--- shell/diskpart.sh
+++ shell/diskpart.sh
@@ -2,5 +2,8 @@
 
 ## pve4
 ## be sure to do --zap-all (not just --zap) to remove GPT ~AND~ MBR stuff
+## the partition seems to start on sector 2048, even without the alignment option
+## the partition table seems to be implicitly created
 
 sgdisk /dev/sda --zap-all
+sgdisk /dev/sda --largest-new=1 --typecode=1:8e00 --change-name=1:"Linux LVM"
