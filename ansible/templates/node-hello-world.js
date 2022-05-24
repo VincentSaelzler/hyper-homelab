a784a67da3f028769dfac998804cc762af100142
Vincent Saelzler <vincent.saelzler@gmail.com>
2022-05-23 22:30:03 -0400

    do not rewrite collatz urls - accept at app level

--- ansible/templates/node-hello-world.js
+++ ansible/templates/node-hello-world.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.send(`Hello from port ${port} on host ${myhost}`)
 })
 
+app.get('/collatz', (req, res) => {
+  res.send(`Greetings integer fans from ${port} on host ${myhost}`)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
